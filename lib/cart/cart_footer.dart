46a9389d93c5145738e13d061a8bea015031c6ed
sundar.kota <sundar.kota@gmail.com>
2020-09-28 08:49:30 +0530

    cart footer page

---
+++ lib/cart/cart_footer.dart
@@ -0,0 +1,62 @@
+
+//import 'dart:async';
+//import 'dart:convert';
+//import 'dart:io';
+/*
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+//import 'package:flutter_wordpress/flutter_wordpress.dart' as wp;
+//import 'package:flutter_html/flutter_html.dart';
+import 'package:greenillu/appconfig/config.dart';
+import 'package:greenillu/widgets/app_bar.dart';
+import 'package:greenillu/widgets/appdrawer.dart';
+//import 'package:greenillu/model/any_image.dart';
+//import 'package:greenillu/model/category.dart';
+import 'package:greenillu/model/cart.dart';
+import 'package:greenillu/model/product.dart';
+import 'package:greenillu/widgets/products_list_item.dart';
+import 'package:greenillu/widgets/carts_list_item.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
+import 'package:greenillu/scoped_model/cart_scoped_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:greenillu/utils/functions.dart';
+import 'package:greenillu/utils/format.dart';
+
+class Cartfooter extends StatefulWidget {
+  CartfooterState createState() => CartfooterState();
+}
+
+class CartfooterState extends State {
+
+  String textHolder = 'Old Sample Text...!!!';
+
+  changeText() {
+
+    setState(() {
+      textHolder = 'New Sample Text...';
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(child: Column(
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: Text('$textHolder',
+                      style: TextStyle(fontSize: 21))),
+
+              RaisedButton(
+                onPressed: () => changeText(),
+                child: Text('Click Here To Change Text Widget Text Dynamically'),
+                textColor: Colors.white,
+                color: Colors.green,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              ),
+
+            ]))
+    );
+  }
+}*/
